6655f4952b7905c4d5199a7af47aac045b7e2dcc
Alex Culea <195758113+alexculealt@users.noreply.github.com>
2025-07-21 21:47:42 +0300

    Add required DB migration for credentials

--- server/migrations/0007_material_albert_cleary.sql
+++
@@ -1,2 +0,0 @@
-ALTER TYPE "catalogue_type" ADD VALUE 'CREDENTIALS';--> statement-breakpoint
-ALTER TYPE "page_type" ADD VALUE 'EXPLORATORY';
---
+++ server/migrations/0007_nappy_paladin.sql
@@ -0,0 +1,4 @@
+ALTER TYPE "catalogue_type" ADD VALUE 'CREDENTIALS';--> statement-breakpoint
+ALTER TYPE "page_type" ADD VALUE 'EXPLORATORY';--> statement-breakpoint
+ALTER TYPE "provider_model" ADD VALUE 'gpt-4.1';--> statement-breakpoint
+ALTER TYPE "provider_model" ADD VALUE 'o4-mini';
