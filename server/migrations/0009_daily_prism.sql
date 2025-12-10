35df08b5f30aac568798a3f3cf91c836eabf44b3
Rômulo Saksida <romulo@rsaksida.com>
2025-12-10 12:19:26 -0300

    Rebuild migration snapshots and journal

--- server/migrations/0009_daily_prism.sql
+++ server/migrations/0009_daily_prism.sql
@@ -1,2 +1,4 @@
+ALTER TABLE "recipes" ADD COLUMN "description" text;--> statement-breakpoint
+ALTER TABLE "recipes" ADD COLUMN "name" text;--> statement-breakpoint
 ALTER TABLE "recipes" ADD COLUMN "is_template" boolean DEFAULT false NOT NULL;--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "recipes_template_idx" ON "recipes" USING btree ("is_template","catalogue_id");
