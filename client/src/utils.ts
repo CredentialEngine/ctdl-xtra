143dd2e3815d746dec2a4812b3fa52cfffab1b9a
Alex Culea <195758113+alexculealt@users.noreply.github.com>
2026-04-17 16:24:43 +0300

    Surface recipe name in extractions and merge stats column into one

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -44,9 +44,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function prettyPrintDate(dateStr: string) {
   const date = new Date(dateStr);
-  return date.toLocaleString("en-US", {
-    month: "long",
-    day: "numeric",
+  return date.toLocaleString(undefined, {
+    month: "2-digit",
+    day: "2-digit",
     year: "numeric",
     hour: "2-digit",
     minute: "2-digit",
