cc32df1a804dc7860e336ea5852d55d93b848c97
Alex Culea <195758113+alexculealt@users.noreply.github.com>
2026-05-04 13:59:16 +0300

    Enable NodeJS inspector when env variable is present

--- server/entrypoint-worker.sh
+++ server/entrypoint-worker.sh
@@ -4,4 +4,8 @@ set -e
 chown -R pptruser:pptruser /app
 chown -R pptruser:pptruser /app/db
 
+if [ -n "$INSPECT_WORKERS" ]; then
+  set -- "$@" --node-args="--inspect=0.0.0.0:9229"
+fi
+
 exec runuser -u pptruser "$@"
