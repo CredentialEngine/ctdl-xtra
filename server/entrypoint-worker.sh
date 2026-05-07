613baf52f09bed4d10292a0f4966cb62299b6ed1
Ariel Rolfo <arielr-lt+username@users.noreply.github.com>
2026-05-07 20:51:06 -0300

    Avoid chowning mounted storage in entrypoints

--- server/entrypoint-worker.sh
+++ server/entrypoint-worker.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 set -e
 
-chown -R pptruser:pptruser /app
-chown -R pptruser:pptruser /app/db
-
 export HOME=/home/pptruser
 export PUPPETEER_CACHE_DIR=/home/pptruser/.cache/puppeteer
+
+mkdir -p "$PUPPETEER_CACHE_DIR"
+for path in /home/pptruser /app/db; do
+  if [ -e "$path" ]; then
+    chown -R pptruser:pptruser "$path"
+  fi
+done
 
 if [ -n "$INSPECT_WORKERS" ]; then
   set -- "$@" --node-args="--inspect=0.0.0.0:9229"
--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -4,7 +4,14 @@ set -e
 echo "Running migrations"
 ./node_modules/.bin/drizzle-kit migrate
 
-chown -R pptruser:pptruser /app
-chown -R pptruser:pptruser /app/db
+export HOME=/home/pptruser
+export PUPPETEER_CACHE_DIR=/home/pptruser/.cache/puppeteer
+
+mkdir -p "$PUPPETEER_CACHE_DIR"
+for path in /home/pptruser /app/db; do
+  if [ -e "$path" ]; then
+    chown -R pptruser:pptruser "$path"
+  fi
+done
 
 exec runuser -u pptruser "$@"
